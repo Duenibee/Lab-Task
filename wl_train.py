5e4a902a27c6c641dddf1ac0433a08f1cbe955d2
hanjunhee <89338327+Duenibee@users.noreply.github.com>
2023-02-01 23:27:19 +0900

    Update wl_train.py

--- wl_train.py
+++ wl_train.py
@@ -61,63 +61,30 @@ trainsets =CustomDataset(train_set_wl,label_train_wl)
 train_loader= DataLoader(trainsets, batch_size=10, shuffle=True)
 
 
-# 학습 모델
-class NeuralNet(nn.Module):
-    def __init__(self):
-        super(NeuralNet,self).__init__()
-        self.bn1=nn.BatchNorm1d(128)
-        self.bn2=nn.BatchNorm1d(128)
-        self.bn3=nn.BatchNorm1d(64)
-
-        self.layer_1 = nn.Linear(12, 128, bias=True)
-        self.layer_2 = nn.Linear(128, 128, bias=True)
-        self.layer_3 = nn.Linear(128, 64, bias=True)
-        self.layer_out = nn.Linear(64, 17, bias=True)
-        self.dropout = nn.Dropout(0.2)
-        self.relu = nn.ReLU()
-        self.tanh=nn.Tanh()
-        
-    def forward(self, x):
-        x = self.layer_1(x)
-        x = self.tanh(x)
-        x= self.bn1(x)
-        
-        x = self.layer_2(x)
-        x = self.tanh(x)
-        x = self.dropout(x)
-        x= self.bn2(x)
-        
-        x = self.layer_3(x)
-        x = self.tanh(x)
-        x = self.dropout(x)
-        x= self.bn3(x)
-
-        x = self.layer_out(x)
-        
-        return x
-
 # 모델생성
-net= NeuralNet().cuda(device)
+net= NeuralNet().to(device)
 
 criterion=nn.CrossEntropyLoss().cuda()
-
+#optimizer= optim.SGD(net.parameters(),lr=0.001,momentum=0.9)
 optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
-total_batch=len(train_loader)
 n=len(train_loader)
+net.train()
 loss_=[]
-for epoch in range(1, 700+1):
+for epoch in range(1,700+1):
     correct=0
     total=0
     running_loss=0.0
-    # i=0
     for data in train_loader:
         inputs,labels=data
         labels=labels.to(device)
         inputs=inputs.to(device)
+        # zero_grad 중복계산을 막기위해 0으로 명시적으로 재설정
         optimizer.zero_grad()
         outputs=net(inputs)
         # labels=labels.long()ArithmeticError
         loss=criterion(outputs.squeeze(),labels.squeeze())
+        # loss.backwards() 호출하여 예측 손실(prediction loss)을 역전파
+        # optimizer.step()을 호출하여 역전파 단계에서 수집된 변화도로 매개변수를 조정
         loss.backward()
         optimizer.step()
         _,predicted=torch.max(outputs.data,1)
@@ -128,7 +95,7 @@ for epoch in range(1, 700+1):
     if epoch%10==0:
         print(f'----- Epoch {epoch} -----')
         print("acuuracy:",100*correct/total)
-        print("loss:",running_loss/n)       
+        print("loss:",running_loss/n)     
         
 print("finished learning")
 PATH="C:/Users/wnsgm/Desktop/sEMG_final/weight/wl_700.pth"
