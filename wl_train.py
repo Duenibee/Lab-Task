9aa9eac0f8d061f7128a526f5f91ce412a59efa8
hanjunhee <89338327+Duenibee@users.noreply.github.com>
2023-02-02 01:43:01 +0900

    Update wl_train.py

--- wl_train.py
+++ wl_train.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader, Dataset
 from torch import nn
 import random
 
-# gpu설정
+#gpu설정
 USE_CUDA=torch.cuda.is_available()
 device=torch.device("cuda" if USE_CUDA else 'cpu')
 print("다음 기기로 학습합니다.", device)
@@ -24,19 +24,17 @@ segement1,index=sEMG().segmentation(x)
 # mav , var ,wl
 mav,var,wl= sEMG().statics_all(segement1,index)
 
-# statics로 train, test set 분류하기
+## statics로 train, test set 분류하기
 train_mav,train_var,train_wl,test_mav,test_var,test_wl=sEMG().trian_test_set(mav, var, wl)
 
 # label 만들기
-label_train_mav,label_test_mav=sEMG().make_label(train_mav, test_mav)
-label_train_var,label_test_var=sEMG().make_label(train_var, test_var)
 label_train_wl,label_test_wl=sEMG().make_label(train_wl, test_wl)
 
 # trian,test 데이터 분류
-train_set_mav,test_set_mav,label_train_mav,label_test_mav=sEMG().real_dataset(train_mav,test_mav,label_train_mav,label_test_mav)
-train_set_var,test_set_var,label_train_var,label_test_var=sEMG().real_dataset(train_var,test_var,label_train_var,label_test_var)
 train_set_wl,test_set_wl,label_train_wl,label_test_wl=sEMG().real_dataset(train_wl,test_wl,label_train_wl,label_test_wl)
 
+# valid_data load
+valid_wl,valid_wl_label,test_set_wl,label_test_wl=sEMG().validtaion_dataset(test_set_wl, label_test_wl)
 
 # data tensor로 불러오기
 class CustomDataset(Dataset): 
@@ -60,17 +58,61 @@ class CustomDataset(Dataset):
 trainsets =CustomDataset(train_set_wl,label_train_wl)
 train_loader= DataLoader(trainsets, batch_size=10, shuffle=True)
 
+# valid 데이터 불러오기
+validsets =CustomDataset(valid_wl,valid_wl_label)
+valid_loader= DataLoader(validsets, batch_size=1, shuffle=False)
 
+def saveModel(): 
+    path = "./pause_mav_model.pth" 
+    torch.save(net.state_dict(), path) 
+    
+# 학습 모델
+class NeuralNet(nn.Module):
+    def __init__(self):
+        super(NeuralNet,self).__init__()
+        self.bn1=nn.BatchNorm1d(128)
+        self.bn2=nn.BatchNorm1d(128)
+        self.bn3=nn.BatchNorm1d(64)
+
+        self.layer_1 = nn.Linear(12, 128, bias=True)
+        self.layer_2 = nn.Linear(128, 128, bias=True)
+        self.layer_3 = nn.Linear(128, 64, bias=True)
+        self.layer_out = nn.Linear(64, 17, bias=True)
+        self.dropout = nn.Dropout(0.2)
+        self.tanh=nn.Tanh()
+        
+    def forward(self, x):
+        x = self.layer_1(x)
+        x= self.bn1(x)
+        x = self.tanh(x)
+        
+        
+        x = self.layer_2(x)
+        x= self.bn2(x)
+        x = self.tanh(x)
+        # x = self.dropout(x)
+        
+        x = self.layer_3(x)
+        x= self.bn3(x)
+        x = self.tanh(x)
+        # x = self.dropout(x)
+
+        x = self.layer_out(x)
+        
+        return x
+    
 # 모델생성
 net= NeuralNet().to(device)
-
 criterion=nn.CrossEntropyLoss().cuda()
 #optimizer= optim.SGD(net.parameters(),lr=0.001,momentum=0.9)
 optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 n=len(train_loader)
-net.train()
+n_val=len(valid_loader)
+best_accuracy=98
 loss_=[]
+val_loss_list=[]
 for epoch in range(1,700+1):
+    net.train()
     correct=0
     total=0
     running_loss=0.0
@@ -81,28 +123,53 @@ for epoch in range(1,700+1):
         # zero_grad 중복계산을 막기위해 0으로 명시적으로 재설정
         optimizer.zero_grad()
         outputs=net(inputs)
-        # labels=labels.long()ArithmeticError
         loss=criterion(outputs.squeeze(),labels.squeeze())
         # loss.backwards() 호출하여 예측 손실(prediction loss)을 역전파
         # optimizer.step()을 호출하여 역전파 단계에서 수집된 변화도로 매개변수를 조정
         loss.backward()
         optimizer.step()
         _,predicted=torch.max(outputs.data,1)
-        total+=labels.size(0)
+        total+=outputs.size(0)
         correct+=(predicted==labels).sum().item()
         running_loss+=loss.item()
     loss_.append(running_loss/n)  
+    
+    # validation
+    with torch.no_grad():
+        net.eval()
+        val_epoch_loss = 0
+        val_correct=0
+        val_total=0
+        for data in valid_loader:
+            inputs_val,labels_val=data
+            labels_val=labels_val.to(device)
+            inputs_val=inputs_val.to(device)
+            outputs_val=net(inputs_val)
+            val_loss = criterion(outputs_val.squeeze(), labels_val.squeeze())
+            _,predicted_val=torch.max(outputs_val.data,1)
+            val_total+=outputs_val.size(0)
+            val_correct+=(predicted_val==labels_val).sum().item()
+            val_epoch_loss+=val_loss.item()
+    if 100*correct/total > best_accuracy: 
+        saveModel() 
+        best_accuracy = 100*correct/total
+        print("stop training early")
+        break
     if epoch%10==0:
-        print(f'----- Epoch {epoch} -----')
-        print("acuuracy:",100*correct/total)
-        print("loss:",running_loss/n)     
+        print("\n")
+        print(f'--------- Epoch {epoch} ----------')
+        print("train_acuuracy:",100*correct/total)
+        print("train_loss:",running_loss/n)       
+        print("---------------------------")
+        print("valid_acuuracy:",100*val_correct/val_total)
+        print("valid_loss:",val_epoch_loss/n_val)  
         
 print("finished learning")
 PATH="C:/Users/wnsgm/Desktop/sEMG_final/weight/wl_700.pth"
 torch.save(net.state_dict(),PATH)
 
 plt.plot(loss_)
-plt.title('Loss')
+plt.title('WL_Loss')
 plt.xlabel('epoch')
 plt.show()
 
